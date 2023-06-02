c73551d3fb2f1dfa72a972c32d04752a142744b9
Kevince05 <cesco.kevin05@gmail.com>
2023-06-02 23:00:51 +0200

    new shaders

--- Assets/earth/scripts/DayNightCycle.cs
+++ Assets/earth/scripts/DayNightCycle.cs
@@ -17,7 +17,7 @@ public class DayNightCycle : MonoBehaviour
         transform.Translate(speed * Time.deltaTime, 0f, 0f);
         if (transform.position.x > 20.5f)
         {
-            transform.position = new Vector3(-20.5f,0f,6f);
+            transform.position = new Vector3(-20.5f,0f,-6f);
         } 
     }
 }
