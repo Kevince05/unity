20da8c52375b827b02d16db65857f14da98ad70f
Kevince05 <cesco.kevin05@gmail.com>
2023-06-02 20:08:48 +0200

    funziona!

--- Assets/planes/script/movement.cs
+++ Assets/planes/script/movement.cs
@@ -5,23 +5,22 @@ using UnityEngine.UI;
 
 public class movement : MonoBehaviour
 {
-    [SerializeField]
-    public float speed;
-    [SerializeField]
-    public float rotation_speed;
+    [SerializeField] public float speed;
+    [SerializeField] public float rotation_speed;
+    [SerializeField] public float min_speed;
     [SerializeField]
     public GameObject Plane;
 
     private Rigidbody Rigidbody;
     void Start()
     {
-
+        Rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Rigidbody.velocity = -Plane.transform.right * Input.GetAxis("Vertical");
+        Rigidbody.velocity = -Plane.transform.right * (Input.GetAxis("Vertical") + 1 + min_speed) * speed;
         Plane.transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * rotation_speed, 0));
     }
 }
