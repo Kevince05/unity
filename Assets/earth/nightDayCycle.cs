cfdc8c0266a148fbb9c9cdb6c1b7ced415808230
Kevince05 <cesco.kevin05@gmail.com>
2023-06-02 12:27:37 +0200

    updated world texture

---
+++ Assets/earth/nightDayCycle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Spin the object at a specified speed
+/// </summary>
+public class nightDayCycle : MonoBehaviour {
+
+	public float speed = 10f;
+
+	[HideInInspector]
+	public bool clockwise = true;
+
+	// Update is called once per frame
+	void Update() {
+
+
+		if (clockwise) {
+			transform.parent.transform.Rotate(Vector3.up, speed * Time.deltaTime);
+		} else {
+			transform.parent.transform.Rotate(-Vector3.up, speed * Time.deltaTime);
+		}
+	}
+}
