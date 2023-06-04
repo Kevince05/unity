2edcc720a85dc5ae276d877ca9fbfaeb3d863231
Kevince05 <cesco.kevin05@gmail.com>
2023-06-04 21:13:50 +0200

    finished pacman

--- Assets/planes/script/pacman.cs
+++ Assets/planes/script/pacman.cs
@@ -1,30 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class pacman : MonoBehaviour
 {
     [SerializeField] public Camera mainCamera;
+    [SerializeField] public float bump_x;
+    [SerializeField] public float bump_y;
+    private bool flipped = false;
     void Start()
     {
-        Debug.Log("w:" + mainCamera.pixelWidth + " h:" + mainCamera.pixelHeight);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(mainCamera.ScreenToWorldPoint(new Vector3(mainCamera.pixelWidth, mainCamera.pixelHeight / 2)).x);
         if(mainCamera.ScreenToWorldPoint(new Vector3(mainCamera.pixelWidth, mainCamera.pixelHeight / 2)).x > 20.5f ||
            mainCamera.ScreenToWorldPoint(new Vector3(0, mainCamera.pixelHeight / 2)).x < -20.5f)
         {
-            Debug.Log("tp_to: " + new Vector3(-transform.position.x, transform.position.y, transform.position.z).ToString());
-            transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-transform.position.x + Mathf.Sign(transform.position.x) * bump_x, transform.position.y, transform.position.z);
         }
-        if(mainCamera.ScreenToWorldPoint(new Vector3(mainCamera.pixelWidth/2, mainCamera.pixelHeight)).y > 10.25f ||
-           mainCamera.ScreenToWorldPoint(new Vector3(mainCamera.pixelWidth / 2, 0)).y < -10.25f)
+
+        if (!flipped)
         {
-            Debug.Log("tp_to: " + new Vector3(-transform.position.x, transform.position.y, transform.position.z).ToString());
-            transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+            if (mainCamera.ScreenToWorldPoint(new Vector3(mainCamera.pixelWidth / 2, mainCamera.pixelHeight)).y > 10.25f || mainCamera.ScreenToWorldPoint(new Vector3(mainCamera.pixelWidth / 2, 0)).y < -10.25f)
+            {
+                transform.position = new Vector3(-transform.position.x, transform.position.y - Mathf.Sign(transform.position.y) * bump_y, transform.position.z);
+                transform.Rotate(0, 0, 180);
+                flipped = true;
+            }
+        }
+        else
+        {
+            if (mainCamera.ScreenToWorldPoint(new Vector3(mainCamera.pixelWidth / 2, mainCamera.pixelHeight)).y < -10.25f || mainCamera.ScreenToWorldPoint(new Vector3(mainCamera.pixelWidth / 2, 0)).y > 10.25f)
+            {
+                transform.position = new Vector3(-transform.position.x, transform.position.y - Mathf.Sign(transform.position.y) * bump_y, transform.position.z);
+                transform.Rotate(0, 0, 180);
+                flipped = false;
+            }
         }
     }
 }
